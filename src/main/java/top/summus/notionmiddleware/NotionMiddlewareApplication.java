6bed562e8447e749d4d0819948e9df6ed85b5581
summuss <zongyang_liu@outlook.com>
2021-02-19 17:39:20 +0800

    init

---
+++ src/main/java/top/summus/notionmiddleware/NotionMiddlewareApplication.java
@@ -0,0 +1,12 @@
+package top.summus.notionmiddleware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotionMiddlewareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotionMiddlewareApplication.class, args);
+    }
+}
---
+++ src/test/java/top/summus/notionmiddleware/NotionMiddlewareApplicationTests.java
@@ -0,0 +1,11 @@
+package top.summus.notionmiddleware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotionMiddlewareApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
